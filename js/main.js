045289a7498f38acb9d413dfb57cc9150e1faac6
Lukasz <lukaszprzybylka@gmail.com>
2022-04-17 16:19:01 +0100

    Added reset game and conversion of values to numbers

--- js/main.js
+++ js/main.js
@@ -10,10 +10,30 @@ if(!(localStorage.getItem('deckID'))) {
       })
     }
 
-document.getElementById('startGame').addEventListener('click', startGame)
+document.getElementById('startGame').addEventListener('click', start)
 
-function startGame() {
-    fetch(`https://deckofcardsapi.com/api/deck/${localStorage.getItem('deckID')}/draw/?count=2`)
+function start() {
+    fetch(`https://deckofcardsapi.com/api/deck/${localStorage.getItem('deckID')}/draw/?count=4`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+      document.querySelector('#cardDealer-1').src = data.cards[0].image
+      document.querySelector('#cardDealer-2').src = data.cards[1].image
+      document.querySelector('#cardPlayer-1').src = data.cards[2].image
+      document.querySelector('#cardPlayer-2').src = data.cards[3].image
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+  }
+
+  document.getElementById('resetGame').addEventListener('click', reset)
+
+  function reset() {
+
+    document.querySelectorAll('cards').style.display = 'none'
+
+    fetch(`https://deckofcardsapi.com/api/deck/${localStorage.getItem('deckID')}/shuffle`)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data)
@@ -21,4 +41,14 @@ function startGame() {
     .catch(err => {
         console.log(`error ${err}`)
     })
+  }
+
+  function convertToNum(val) {
+    if(val === 'ACE') {
+        return 11
+    } else if (val === 'KING' || val === 'QUEEN' || val === 'JACK') {
+        return 10
+      } else {
+        return Number(val)
+      }
   }
