0948661cf41be65a9f1291742d25ddd16d70882e
Lukasz <lukaszprzybylka@gmail.com>
2022-04-17 15:34:48 +0100

    Add functionality to draw cards

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ if(!(localStorage.getItem('deckID'))) {
           console.log(`error ${err}`)
       })
     }
+
+document.getElementById('startGame').addEventListener('click', startGame)
+
+function startGame() {
+    fetch(`https://deckofcardsapi.com/api/deck/${localStorage.getItem('deckID')}/draw/?count=2`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    })
+  }
