7b74018bef6eaf1388ad37cb690c76f77c6775be
Lukasz <lukaszprzybylka@gmail.com>
2022-04-18 16:02:37 +0100

    Added betting functionality, expanded option reset to reset players bank, state of the game and input

--- js/main.js
+++ js/main.js
@@ -33,52 +33,71 @@ if(!(localStorage.getItem('playerCount'))) {
     localStorage.setItem('playerCount', 0)
 }
 
+if(!(localStorage.getItem('playerMoney'))) {
+    localStorage.setItem('playerMoney', 5000)
+}
+
+if(!(localStorage.getItem('gameOn'))) {
+    localStorage.setItem('gameOn', 'false')
+}
+
+document.getElementById('wallet').innerText = localStorage.playerMoney
+
 document.getElementById('start').addEventListener('click', start)
 
 function start() {
-    fetch(`https://deckofcardsapi.com/api/deck/${localStorage.getItem('deckID')}/draw/?count=4`)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-          console.log(data)
-          //Dealer
-          document.querySelector('#cardDealer-1').src = data.cards[0].image
-          document.querySelector('#cardDealer-1').style.display = 'inline'
-          document.querySelector('#cardDealer-2').src = data.cards[1].image
-          document.querySelector('#cardDealer-2').style.display = 'inline'
+ 
+  if(localStorage.gameOn === 'true') {
+        alert("The game is already running")
+    } else {
+    localStorage.setItem('gameOn', 'true')
+    let bet = document.getElementById('betAmount').value
+    if(bet != 0) {
+        let money = localStorage.playerMoney
+        localStorage.setItem('playerMoney', money - bet)
+        document.getElementById('wallet').innerText = localStorage.playerMoney
+        fetch(`https://deckofcardsapi.com/api/deck/${localStorage.getItem('deckID')}/draw/?count=4`)
+            .then(res => res.json()) // parse response as JSON
+            .then(data => {
+              //Dealer
+              document.querySelector('#cardDealer-1').src = data.cards[0].image
+              document.querySelector('#cardDealer-1').style.display = 'inline'
+              document.querySelector('#cardDealer-2').src = data.cards[1].image
+              document.querySelector('#cardDealer-2').style.display = 'inline'
+              let dealerScore = convertToNum(data.cards[0].value) + convertToNum(data.cards[1].value)
+              localStorage.setItem('dealerScore', dealerScore)
+              let dealerCards = [data.cards[0].value, data.cards[1].value]
+              localStorage.setItem('dealerCards', dealerCards)
+              localStorage.setItem('dealerCount', 2)
 
-          let dealerScore = convertToNum(data.cards[0].value) + convertToNum(data.cards[1].value)
-          localStorage.setItem('dealerScore', dealerScore)
-
-          let dealerCards = [data.cards[0].value, data.cards[1].value]
-          localStorage.setItem('dealerCards', dealerCards)
-
-          localStorage.setItem('dealerCount', 2)
+              //Player
+              document.querySelector('#cardPlayer-1').src = data.cards[2].image
+              document.querySelector('#cardPlayer-1').style.display = 'inline'
+              document.querySelector('#cardPlayer-2').src = data.cards[3].image
+              document.querySelector('#cardPlayer-2').style.display = 'inline'
+              let playerScore = convertToNum(data.cards[2].value) + convertToNum(data.cards[3].value)
+              localStorage.setItem('playerScore', playerScore)
+              let playerCards = [data.cards[2].value, data.cards[3].value]
+              localStorage.setItem('playerCards', playerCards)
+              localStorage.setItem('playerCount', 2)
             
-          //Player
-          document.querySelector('#cardPlayer-1').src = data.cards[2].image
-          document.querySelector('#cardPlayer-1').style.display = 'inline'
-          document.querySelector('#cardPlayer-2').src = data.cards[3].image
-          document.querySelector('#cardPlayer-2').style.display = 'inline'
-
-          let playerScore = convertToNum(data.cards[2].value) + convertToNum(data.cards[3].value)
-          localStorage.setItem('playerScore', playerScore)
-
-          let playerCards = [data.cards[2].value, data.cards[3].value]
-          localStorage.setItem('playerCards', playerCards)
-
-          localStorage.setItem('playerCount', 2)
-          
-          if(checkBlackjack(localStorage.playerCards)) {
-              if(checkBlackjack(localStorage.dealerCards)) {
-                  return alert("It's a tie")
+              if(checkBlackjack(localStorage.playerCards)) {
+                  if(checkBlackjack(localStorage.dealerCards)) {
+                      return alert("It's a tie")
+                  }
+                  localStorage.setItem('playerMoney', Number(money) + (bet * 2))
+                  document.getElementById('wallet').innerText = localStorage.playerMoney
+                  return alert("BLACKJACK! You win!")
               }
-              return alert("BLACKJACK! You win!")
-          }
 
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-         })
+            })
+            .catch(err => {
+                console.log(`error ${err}`)
+             })
+    } else {
+        alert('You need to place a bet')
+}
+} 
 }
 
 document.getElementById('hit').addEventListener('click', hit)
@@ -104,14 +123,11 @@ document.getElementById('reset').addEventListener('click', reset)
 
 function reset() {
     document.querySelectorAll('.cards').forEach(el => {el.style.display = 'none'})
+    localStorage.setItem('playerMoney', 5000)
+    localStorage.setItem('gameOn', false)
+    document.getElementById('betAmount').value = ''
+    document.getElementById('wallet').innerText = localStorage.playerMoney
     fetch(`https://deckofcardsapi.com/api/deck/${localStorage.getItem('deckID')}/shuffle`)
-        .then(res => res.json()) // parse response as JSON
-        .then(data => {
-          console.log(data)
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-    })
 }
 
 function convertToNum(val) {
